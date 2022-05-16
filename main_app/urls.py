3c7b44dbc9c31b93e57eaee2d3eaf2023f39c14f
devoperations <chrishayes0130@gmail.com>
2022-05-16 16:34:31 -0700

    add recipe

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('recipes/', views.recipes, name='recipes'),
     path('recipes/create/', views.RecipeCreate.as_view(), name='recipes_create'),
-    
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', views.signup, name='signup'),
+    path('recipes/<int:pk>/', views.recipe_detail, name='detail')
 ]
