f04bbc647560091e9dd9145262b0828a52f82ba0
iballinger <ilerdonballinger@gmail.com>
2022-05-17 10:19:17 -0700

    Review Model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Recipe, Photo
+from .models import Recipe, Photo, Review
 # Register your models here.
 admin.site.register(Recipe)
 admin.site.register(Photo)
+admin.site.register(Review)
